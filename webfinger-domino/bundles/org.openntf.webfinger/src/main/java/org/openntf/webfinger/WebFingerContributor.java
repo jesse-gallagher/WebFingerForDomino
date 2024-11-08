c386452ae27d954545e719ce38eb5e69a4ad62d0
Jesse Gallagher <jesse@secondfoundation.org>
2024-11-08 10:50:51 -0500

    Expand Javadoc

--- webfinger-domino/bundles/org.openntf.webfinger/src/main/java/org/openntf/webfinger/WebFingerContributor.java
+++ webfinger-domino/bundles/org.openntf.webfinger/src/main/java/org/openntf/webfinger/WebFingerContributor.java
@@ -7,11 +7,26 @@ import java.util.Map;
 import com.ibm.commons.util.io.json.JsonException;
 import com.ibm.commons.util.io.json.JsonObject;
 
+/**
+ * This extension interface allows classes to contribute to the result
+ * profile JSON.
+ * 
+ * @since 2.0.0
+ */
 public interface WebFingerContributor {
+	/**
+	 * Called to retrieve a collection of item names to request
+	 * from the directory.
+	 * 
+	 * @return a {@link Collection} of strings; cannot be {@code null}
+	 */
 	Collection<String> getItems();
 	
 	/**
 	 * Called to ask the contributor to add its information to the result payload.
+	 * 
+	 * <p>When this is called, {@code root} is guaranteed to contain a {@code "subject"}
+	 * string, a {@code "links"} array, and an {@link "aliases"} array.</p>
 	 * 
 	 * @param items a {@link Map} of the requested items to their values from the directory
 	 * @param root the root JSON result object
